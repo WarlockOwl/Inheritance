dd07bb351cf5ab8bca014b3927fa86fb5f0ca3fa
Aaron Richardson <aaron.richardson1@sinclair.edu>
2024-11-29 13:50:27 -0500

    Added sections for the use of the Car and Truck classes

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -25,9 +25,39 @@ int main() {
 
 	cout << "Vehicle Information: " << endl;
 	cout << "Manufacturer: " << entry1.getManu() << endl;
-	cout << "Year Built: " << entry1.getYear() << endl;
+	cout << "Year Built: " << entry1.getYear() << endl << endl;
 
+	cout << "Car" << endl;
+	cout << "Enter the manufacturer: ";
+	cin >> tempVehicle;
+	entry2.setManu(tempVehicle);
+	cout << "Enter the year built: ";
+	cin >> tempNum;
+	entry2.setYear(tempNum);
+	cout << "Enter the number of doors: ";
+	cin >> tempNum;
+	entry2.setDoors(tempNum);
 
+	cout << "Vehicle Information: " << endl;
+	cout << "Manufacturer: " << entry2.getManu() << endl;
+	cout << "Year Built: " << entry2.getYear() << endl;
+	cout << "Doors: " << entry2.getDoors() << endl;
+
+	cout << "Truck" << endl;
+	cout << "Enter the manufacturer: ";
+	cin >> tempVehicle;
+	entry2.setManu(tempVehicle);
+	cout << "Enter the year built: ";
+	cin >> tempNum;
+	entry3.setYear(tempNum);
+	cout << "Enter the number of doors: ";
+	cin >> tempNum;
+	entry3.setTow(tempNum);
+
+	cout << "Vehicle Information: " << endl;
+	cout << "Manufacturer: " << entry3.getManu() << endl;
+	cout << "Year Built: " << entry3.getYear() << endl;
+	cout << "Doors: " << entry3.getTow() << endl;
 
 	return 0;
 }
