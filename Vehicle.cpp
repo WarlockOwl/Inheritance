09904c30bc732adb407c8ba7e551896f238d89f9
Aaron Richardson <aaron.richardson1@sinclair.edu>
2024-12-01 11:09:08 -0500

    Added comments

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -22,10 +22,12 @@ int main() {
 	cout << "Enter the year built: ";
 	cin >> tempNum;
 	entry1.setYear(tempNum);
+	//lets the user enter the vehicle information
 
 	cout << "Vehicle Information: " << endl;
 	cout << "Manufacturer: " << entry1.getManu() << endl;
 	cout << "Year Built: " << entry1.getYear() << endl << endl;
+	//prints the entered vehicle information
 
 	cout << "Car" << endl;
 	cout << "Enter the manufacturer: ";
@@ -37,11 +39,13 @@ int main() {
 	cout << "Enter the number of doors: ";
 	cin >> tempNum;
 	entry2.setDoors(tempNum);
+	//lets the user enter the car information
 
 	cout << "Vehicle Information: " << endl;
 	cout << "Manufacturer: " << entry2.getManu() << endl;
 	cout << "Year Built: " << entry2.getYear() << endl;
 	cout << "Doors: " << entry2.getDoors() << endl;
+	//prints the entered car information
 
 	cout << "Truck" << endl;
 	cout << "Enter the manufacturer: ";
@@ -53,11 +57,13 @@ int main() {
 	cout << "Enter the number of doors: ";
 	cin >> tempNum;
 	entry3.setTow(tempNum);
+	//lets the user enter the truck information
 
 	cout << "Vehicle Information: " << endl;
 	cout << "Manufacturer: " << entry3.getManu() << endl;
 	cout << "Year Built: " << entry3.getYear() << endl;
 	cout << "Doors: " << entry3.getTow() << endl;
+	//prints the entered truck information
 
 	return 0;
 }
--- Vehicle.h
+++ Vehicle.h
@@ -21,27 +21,31 @@ public:
 		manufacturer = " ";
 		year = 0;
 	}
+	//sets default values
+
 	void setManu(string m) {
 		manufacturer = m;
 	}
 	string getManu() {
 		return manufacturer;
 	}
-
 	void setYear(int y) {
 		year = y;
 	}
 	int getYear() {
 		return year;
 	}
+	//the functions called to set and get the manufacturer and year produced
 
 
 	void displayInfo() {
 		cout << "Manufacturer: " << manufacturer << endl;
 		cout << "year Built: " << year << endl;
 	}
+	//Displays the entered info of the vehicle
 
 };
+//Class for vehicles in general, and will be inherited by the subsequent classes
 
 class Car: public Vehicle {
 protected:
@@ -50,19 +54,23 @@ public:
 	Car() {
 		doors = 0;
 	}
+	//sets default values
 	void setDoors(int d) {
 		doors = d;
 	}
 	int getDoors() {
 		return doors;
 	}
+	//the functions called to set and get the towing cap
 
 	void displayInfo() {
 		cout << "Manufacturer: " << manufacturer << endl;
 		cout << "year Built: " << year << endl;
 		cout << "Doors: " << doors << endl;
 	}
+	//Displays the entered info of the car
 };
+//class for specifically cars and their values, inherits the vehicle class
 
 class Truck : public Vehicle {
 protected:
@@ -71,17 +79,21 @@ public:
 	Truck() {
 		towingCap = 0;
 	}
+	//sets default values
 	void setTow(int t) {
 		towingCap = t;
 	}
 	int getTow() {
 		return towingCap;
 	}
+	//the functions called to set and get the towing cap
 
 	void displayInfo() {
 		cout << "Manufacturer: " << manufacturer << endl;
 		cout << "year Built: " << year << endl;
 		cout << "Towing capacity: " << towingCap << endl;
 	}
+	//Displays the entered info of the truck
 };
+//class for specifically trucks and their values, inherits the vehicle class
 #endif // !1
